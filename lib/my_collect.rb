d800548c59bac88b06d2b3b2c900794c1d2c485a
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-18 05:16:42 -0500

    commit tree 6dc97890764fa901c67ebae10110d057f23bf63a

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+# languages =["ruby", "javascript", "python", "objective-c"]
+# students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
 
+def my_collect(array)
+empty_array = []
+counter = 0
+collection = []
+
+while counter < array.length
+    collection << yield(array[counter])
+    counter += 1
+      end 
+    collection
+end  
+
+# my_collect(languages)
+# {|lang| lang.upcase} 
+
+# my_collect(students)
+# {|name|name.split(" ").first }
